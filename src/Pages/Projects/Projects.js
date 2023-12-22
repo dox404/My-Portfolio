7c06de1e835ecb1f1b61dc3ce6ccba0640634fb3
dox404 <muktarul.hoque@hotmail.com>
2023-12-22 19:06:40 +0530

    fixed the card component in project section

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,65 +1,86 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Projects.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+
+
+//extra 
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+
 function Projects() {
     //  const [proj,setProj]=useState(" ")
-        let projects = [
-            {
-                id: "01",
-                prjectName: "News App",
-                description: "zzzzzzzzz",
-                TechStack:"yehbdn"
+    let projects = [
+        {
+            id: "01",
+            prjectName: "News App",
+            description: "zzzzzzzzz",
+            TechStack: "React Js"
 
-            },
-            {
-                id: "02",
-                prjectName: "News",
-                description: "zzzzzzzzz",
-                TechStack:"yehbdn"
-            },
-            {
-                id: "03",
-                prjectName: "Ngyfrn",
-                description: "zzzzihfnezzzzz",
-                TechStack:"yehbdn"
-            },
+        },
+        {
+            id: "02",
+            prjectName: "E commerce Website",
+            description: "zzzzzzzzz",
+            TechStack: "React Js, Node Js,Bootatrp,Mongo Db,AWS"
+        },
+        {
+            id: "02",
+            prjectName: "E commerce Website",
+            description: "zzzzzzzzz",
+            TechStack: "React Js, Node Js,Bootatrp,Mongo Db,AWS"
+        },
+        {
+            id: "02",
+            prjectName: "E commerce Website",
+            description: "zzzzzzzzz",
+            TechStack: "React Js, Node Js,Bootatrp,Mongo Db,AWS"
+        },
 
-        ]
-        // setProj(projects)
-        // console.log(proj)
+    ]
 
     return (
         <>
             <section id='projects'>
 
 
-
                 <div className="Projects-main">
-                    <h1>Projects</h1>
-                    {
-                        projects.map((value) => {
-                                return (
-                                    <div className="card">
-                                    <Card key={value.id}>
-                                        <Card.Img variant="top" src={"https://picsum.photos/200/300"
-                                        } />
-                                        <Card.Body>
-                                            <Card.Title>{value.prjectName}</Card.Title>
-                                            <Card.Text>
-                                                {value.description}
-                                            </Card.Text> 
-                                            <Card.Text>
-                                                {value.description}
-                                            </Card.Text>
-                                            <Button className='btn btn-primary' href={value.url} target="_blank">Project Link</Button>
-                                            <Button className='btn btn-primary' href={value.url} target="_blank">Live Url</Button>
-                                        </Card.Body>
-                                    </Card>
-                                    </div>
-                                )
-                            })} 
+                    <div className="container">
+                        <div className="row">
+                            <h1 className="text-center page-title">Projects</h1>
+                            {
+                                projects.map((value) => {
+                                    return (
+                                        <div className="col-md-4 my-5 ">
+                                            <div className="card">
+                                                <Card key={value.id}>
+                                                    <Card.Body>
+                                                        <Card.Title>{value.prjectName}</Card.Title>
+                                                        <Card.Text>
+                                                            {value.TechStack}
+                                                        </Card.Text>
+                                                        <Card.Text>
+                                                            {value.description}
+                                                        </Card.Text>
+                                                        <Button className='btn btn-primary' href={value.url} target="_blank">Project Link</Button>
+                                                        <Button className='btn btn-primary' href={value.url} target="_blank">Live Url</Button>
+                                                    </Card.Body>
+                                                </Card>
+                                            </div>
+                                        </div>
+
+
+
+
+                                    )
+                                })}
+                        </div>
+                    </div>
                 </div>
+
+
+
             </section>
         </>
 
