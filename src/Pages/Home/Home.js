58e49d453b18603400072ee1c4f52b9d7964705c
dox404 <muktarul.hoque@hotmail.com>
2024-08-02 23:32:26 +0530

    after long time commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,9 +12,8 @@ function Home() {
                         <div className='container'>
                             <div className='row content'>
                                 <div className="Intro col-6">
-                                    <h1>Hi, I am 
+                                    <h1>Hi, I am
                                         <span style={{ color: 'coral', fontWeight: 'bold' }}>
-
                                             <Typewriter
                                                 words={[' Muktarul Hoque']}
                                                 loop={5}
@@ -23,28 +22,16 @@ function Home() {
                                                 typeSpeed={50}
                                                 deleteSpeed={50}
                                                 delaySpeed={1000}
-                                               
                                             />
                                         </span>
-
-
-
-
-
-
-
-
-
                                     </h1>
-                                    <p style={{'margin-top':'10px'}}>I am a Full Stack Developer With the experice with node,react,mongo."Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    <p style={{ 'margin-top': '10px' }}>I am a Full Stack Developer With the experice with node,react,mongo."Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                                 </div>
                                 <div className="photo col-6">
                                     <img className='img' src={require('./Photos/IMG_20231201_213544.jpg')} />
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
                 </main>
             </section>
